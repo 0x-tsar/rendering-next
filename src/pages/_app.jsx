7f770754ccd9b2284ee93d60301986cf2ca5a29e
Gustavo <contato.gustavofernandes@hotmail.com>
2022-02-27 05:43:49 -0300

    fixing bugs

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,16 @@
 import "../../styles/globals.css";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: green;
+  color: white;
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
